0826ac4ec34d39d681c0ad2bfc5857974cb77f7c
Jeremy <the.jeremy.cobb@gmail.com>
2024-01-07 15:49:39 -0600

    updates

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,17 +1,31 @@
-import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+// import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+import { PrettyChatWindow } from 'react-chat-engine-pretty'
+
 const key = import.meta.env.VITE_PROJECT_KEY;
 
 const ChatsPage = (props) => {
-    const chatProps = useMultiChatLogic(
-        key, 
-        props.user.username, 
-        props.user.secret
-    );
+    // const chatProps = useMultiChatLogic(
+    //     key, 
+    //     props.user.username, 
+    //     props.user.secret
+    // );
     return (
+        // react-chat-engine-advanced ui
+        // <div style={{ height: '100vh' }}>
+        //     <MultiChatSocket { ...chatProps } />
+        //     <MultiChatWindow { ...chatProps} style={{ height: '100%' }} />
+        // </div>
+
+        // pretty chat window ui
         <div style={{ height: '100vh' }}>
-            <MultiChatSocket { ...chatProps } />
-            <MultiChatWindow { ...chatProps} style={{ height: '100%' }} />
+            <PrettyChatWindow
+                projectId={key}
+                username={props.user.username}
+                secret={props.user.secret}
+                style={{ height: '100%' }}
+            />
         </div>
+
     )
 }
 
