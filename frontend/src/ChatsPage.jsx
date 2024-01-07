b683ce53e230ec82406613817cbe299323757b03
Jeremy <the.jeremy.cobb@gmail.com>
2024-01-06 22:05:42 -0600

    finished chats window

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,5 +1,18 @@
-const ChatsPage = () => {
-    return <>chats...</>
+import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
+const key = import.meta.env.VITE_PROJECT_KEY;
+
+const ChatsPage = (props) => {
+    const chatProps = useMultiChatLogic(
+        key, 
+        props.user.username, 
+        props.user.secret
+    );
+    return (
+        <div style={{ height: '100vh' }}>
+            <MultiChatSocket { ...chatProps } />
+            <MultiChatWindow { ...chatProps} style={{ height: '100%' }} />
+        </div>
+    )
 }
 
 export default ChatsPage
