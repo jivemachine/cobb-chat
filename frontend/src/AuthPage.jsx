4dec37bb9b9f2a81cf74c10f6c4fdb3f550f5146
Jeremy <the.jeremy.cobb@gmail.com>
2024-01-06 22:05:22 -0600

    added axios to proj

--- frontend/src/AuthPage.jsx
+++ frontend/src/AuthPage.jsx
@@ -1,11 +1,11 @@
-import axios from 'axios'
+import axios from 'axios';
 
 const AuthPage = (props) => {
   const onSubmit = (e) => {
     e.preventDefault();
     const { value } = e.target[0];
     axios.post(
-        "http:localhost:3001/authenticate", 
+        "http://localhost:3001/authenticate", 
         { username: value }
     )
     .then(r => props.onAuth({ ...r.data, secret: value }))
